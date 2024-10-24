8771116c069c9fc01f2efd2127ad83b0e044f4e5
Sergey Khmelnitsky <154054760+sobchak-77@users.noreply.github.com>
2024-10-24 13:33:45 +0200

    Update img_card .js

--- js/img_card .js
+++ js/img_card .js
@@ -7,12 +7,12 @@ export class ImgCard extends Card {
     this._number = value;
 
     const paramClass = 'game__card-img';
-    const paramSrc = `sobchak-77.github.io/img/sock_${value}.svg`;
+    const paramSrc = `/sobchak-77.github.io/img/sock_${value}.svg`;
     const paramAlt = 'Sock';
 
     this.img = getImg(paramClass, paramSrc, paramAlt);
     this.img.onerror = () => {
-      this.img.src = 'sobchak-77.github.io/img/no_sock.png';
+      this.img.src = '/sobchak-77.github.io/img/no_sock.png';
       throw new Error('You have problems with image loading');
     };
 
